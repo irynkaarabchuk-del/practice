8d1f708d4216d11c06adc5980de81e303a06c52b
IrynkaArabeska <irynkaarabchuk@gmail.com>
2026-06-11 20:31:23 +0300

    add section testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,8 @@ iconBlock.addEventListener('click', () => {
     iconClose.classList.toggle('d-none')
     mainMenu.classList.toggle('menu-open')
 })
+document.querySelectorAll('*').forEach(el => {
+  if (el.scrollWidth > document.documentElement.clientWidth) {
+    console.log(el);
+  }
+});
