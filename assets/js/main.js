754890fcd6e909c022f6aac98c3094b1a40fbbd7
IrynkaArabeska <irynkaarabchuk@gmail.com>
2026-06-12 21:24:48 +0300

    add modal window sign in

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,23 @@ iconBlock.addEventListener('click', () => {
     iconClose.classList.toggle('d-none')
     mainMenu.classList.toggle('menu-open')
 })
+
+
+// Modal
+const exploreBtn = document.getElementById('exploreBtn');
+const modalOverlay = document.getElementById('modalOverlay');
+const modalClose = document.getElementById('modalClose');
+
+exploreBtn.addEventListener('click', () => {
+    modalOverlay.classList.add('active');
+});
+
+modalClose.addEventListener('click', () => {
+    modalOverlay.classList.remove('active');
+});
+
+modalOverlay.addEventListener('click', (e) => {
+    if (e.target === modalOverlay) {
+        modalOverlay.classList.remove('active');
+    }
+});
